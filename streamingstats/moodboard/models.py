1fc842c0653c6c528d426e68892a147d77122591
anna-liang <annaliang56@gmail.com>
2020-12-28 16:05:48 -0500

    Added mood board with embedded audio

---
+++ streamingstats/moodboard/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class MoodBlock(models.Model):
+    track = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    img = models.CharField(max_length=200)
+    audio = models.FileField()
---
+++ streamingstats/moodboard/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from .apps import spotifyObj
+
+from .models import MoodBlock
+
+# Create your views here.
+def index(request):
+    uniqueAlbums = []
+    topTracks = []
+    results = spotifyObj.current_user_top_tracks(time_range='medium_term')['items']
+    print(results)
+    for track in results:
+        if track['album']['name'] not in uniqueAlbums and len(uniqueAlbums) < 9:
+            mb = MoodBlock()
+            mb.album = track['album']['name']
+            mb.artist = track['artists'][0]['name']
+            mb.track = track['name']
+            mb.audio = "https://open.spotify.com/embed/track/" + track['id']
+            print(mb.audio)
+            for image in track['album']['images']:
+                if image['height'] == 300:
+                    mb.img = image['url']
+            uniqueAlbums.append(mb.album)
+            topTracks.append(mb)
+        if len(uniqueAlbums) == 9:
+            break
+    return render(request, 'moodboard/index.html', {'moodboard': 'mood board', 'topTracks': topTracks})
