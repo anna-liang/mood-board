32501bad091f4360db0884de3cfb6ebd764b9974
anna-liang <annaliang56@gmail.com>
2020-12-30 14:47:04 -0500

    Changed authorization flow

--- streamingstats/moodboard/apps.py
+++ streamingstats/moodboard/apps.py
@@ -1,13 +1,4 @@
 from django.apps import AppConfig
 
-import os
-
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-scope = "user-top-read"
-
-spotifyObj = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
 class MoodboardConfig(AppConfig):
     name = 'moodboard'
